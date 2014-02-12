a5c94fea9c36512f4c1f02952b766f50367a5912
Mike Francis <ungrim97@gmail.com>
2014-02-12 17:24:20 +0000

    WIP: Load Test classes

--- lib/Test/Suite.pm
+++ lib/Test/Suite.pm
@@ -1,11 +1,68 @@
 package Test::Suite;
 use 5.008005;
-use strict;
-use warnings;
+use Moose;
+
+use File::Spec::Functions qw/catdir/;
+use FindBin qw/$Bin/;
 
 our $VERSION = "0.01";
 
+has test_dirs => (
+    is  => 'rw',
+    isa => 'Str',
+    default => sub {
+        return [ catdir($Bin, 't', 'Tests') ];
+    },
+);
 
+has test_classes => (
+    is => 'rw',
+    isa => 'ArrayRef',
+    lazy_build => 1,
+);
+
+has exclude_tests => (
+    is => 'rw',
+    isa => 'ArrayRef[Str]',
+    default => sub {[]};
+);
+
+sub runtests {
+    my ($self) = @_;
+
+    my $builder = $self->builder;
+    for my $test_class ($self->test_classes){
+        $builder->subtest(
+            $test_class,
+            $test->run_tests(test_args => $self->test_args);
+        );
+    }
+}
+
+sub filter_test_classes {
+    my ($self, $directory, $test_classes) = @_;
+
+    return sub {
+        return unless $File::Find::name =~ /\.pm$/;
+
+        
+    };
+}
+
+sub _build_test_classes {
+    my ($self) = @_;
+
+    my $test_classes = [];
+
+    for my $directory ($self->test_dirs){
+        $directory = catdir(split '/', $directory);
+        find(
+            no_chdir    => 1,
+            wanted      => \filter_test_class($directory, $test_classes));
+    }
+
+    return $test_classes;
+}
 
 1;
 __END__
@@ -19,6 +76,7 @@ Test::Suite - It's new $module
 =head1 SYNOPSIS
 
     use Test::Suite;
+    
 
 =head1 DESCRIPTION
 
