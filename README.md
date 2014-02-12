db21653843df8fbb2627d06a4a095297a563ba63
Mike Francis <ungrim97@gmail.com>
2014-02-12 16:46:13 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::Suite - It's new $module
+
+# SYNOPSIS
+
+    use Test::Suite;
+
+# DESCRIPTION
+
+Test::Suite is ...
+
+# LICENSE
+
+Copyright (C) Mike Francis.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Mike Francis <ungrim97@gmail.com>
---
+++ lib/Test/Suite.pm
@@ -0,0 +1,39 @@
+package Test::Suite;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Suite - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Suite;
+
+=head1 DESCRIPTION
+
+Test::Suite is ...
+
+=head1 LICENSE
+
+Copyright (C) Mike Francis.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Mike Francis E<lt>ungrim97@gmail.comE<gt>
+
+=cut
+
