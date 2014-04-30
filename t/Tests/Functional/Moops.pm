d22b67cc2706668ddf3cdf34d6f8a2440d93a768
Mike Francis <ungrim97@gmail.com>
2014-04-30 15:24:00 +0100

    Added Load test

--- t/Tests/Functional/Moops.pm
+++ t/Tests/Functional/Moops.pm
@@ -2,6 +2,11 @@ use Moops;
 
 class Tests::Functional::Moops with Test::Suite::Role::Test {
     use Test::Modern;
+
+    BEGIN {
+        ok(1, 'Correctly loaded '.__PACKAGE__);
+    }
+
     sub test_test1 {
         ok(1, 'Tests::Functional::Moops::test_test1 run correctly');
     }
